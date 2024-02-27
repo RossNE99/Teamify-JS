6687f307365625265c7bf2bddd2fe2d0284b3ca2
Ross <rossycharlton@googlemail.com>
2024-02-27 17:05:51 +0000

    Added comments

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+//imports
 import {getTeamDetails} from "./src/get-user-input.js"
 import {render} from "./src/page-template.js";
 import {writeToFile} from "./src/save-html-file.js"
 import path from "path";
 const __dirname = import.meta.dirname;
 
-const init = async () => {
-    const OUTPUT_DIR = path.resolve(__dirname, "output");
-    const outputPath = path.join(OUTPUT_DIR, "team.html");
-    
-    const team = await getTeamDetails()
-    const htmlData = render(team)
+const init = async () => {  // function to initialize program
+    const OUTPUT_DIR = path.resolve(__dirname, "output");   //set the output directory
+    const outputPath = path.join(OUTPUT_DIR, "team.html");  //set the output filepath
 
-    writeToFile(outputPath, OUTPUT_DIR, htmlData)
+    const team = await getTeamDetails() //ask the user for input and wait for the user to finnish putting in the team details
+    const htmlData = render(team)   //Convert users input into a html string
+
+    writeToFile(outputPath, OUTPUT_DIR, htmlData)   //save the html code into a .html file so it can be viewed in a web browser
 }
-init()
+init()  // function call to initialize program
